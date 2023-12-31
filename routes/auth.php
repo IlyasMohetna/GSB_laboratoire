4542852225180f83c955359058cb83265f0a6cd4
ilyas mohetna <ilyas.mohetna@hotmail.com>
2023-12-31 21:53:49 +0100

    fix bug

--- routes/auth.php
+++ routes/auth.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['guest'])->group(function () {
     Route::get('/login', function () {return view('login');})->name('auth.login.show');
     Route::post('/login', [LoginController::class , 'authenticate'])->name('auth.authenticate');
-    Route::get('/logout', [LoginController::class, 'logout'])->name('auth.logout');
 });
+
+Route::get('/logout', [LoginController::class, 'logout'])->middleware('auth')->name('auth.logout');
