ce198aed10fa1d6338e39c5e9668643e6c26e80b
ilyas mohetna <ilyas.mohetna@hotmail.com>
2023-12-31 21:47:29 +0100

    Ajout Middleware redirect if login

--- routes/auth.php
+++ routes/auth.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function () {return view('login');})->name('auth.login.show');
-Route::post('/login', [LoginController::class , 'authenticate'])->name('auth.authenticate');
-Route::get('/logout', [LoginController::class, 'logout'])->name('auth.logout');
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', function () {return view('login');})->name('auth.login.show');
+    Route::post('/login', [LoginController::class , 'authenticate'])->name('auth.authenticate');
+    Route::get('/logout', [LoginController::class, 'logout'])->name('auth.logout');
+});
