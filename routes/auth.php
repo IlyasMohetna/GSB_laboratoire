c5bdc5e67079a220f2945f5805da90327ea3c532
ilyas mohetna <ilyas.mohetna@hotmail.com>
2024-01-01 20:30:20 +0100

    redirection de / vers login

--- routes/auth.php
+++ routes/auth.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest'])->group(function () {
-    Route::get('/login', function () {return view('login');})->name('auth.login.show');
+    Route::get('/{login?}', function () {return view('login');})->name('auth.login.show');
     Route::post('/login', [LoginController::class , 'authenticate'])->name('auth.authenticate');
 });
 
